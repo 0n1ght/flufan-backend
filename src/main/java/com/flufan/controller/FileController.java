4a61df23c343a683fa822f66fb82f44a0ad897e5
0n1ght <carlkasperek@proton.me>
2025-12-08 13:01:14 +0100

    Update todo note in FileController

--- src/main/java/com/flufan/controller/FileController.java
+++ src/main/java/com/flufan/controller/FileController.java
@@ -55,11 +55,11 @@ public class FileController {
 
     //TODO: update pfp
     // sprawdza dane zdjecia, parametry itp. jezeli nie jest odpowiedzie -> zwraca wyjatek
-    // jezeli jest juz zdjecie pod sciezka -> <nickname_zalogowanego_uzytkownika>_pfp.png/jpg to je usowa
-    // zapisuej zdjecie pod sciezka <nickname_zalogowanego_uzytkownika>_pfp.png/jpg
+    // jezeli jest juz zdjecie pod sciezka -> <nickname_zalogowanego_uzytkownika>_pfp.png/jpg/jpeg to je usowa
+    // zapisuej zdjecie pod sciezka <nickname_zalogowanego_uzytkownika>_pfp.png/jpg/jpeg
 
     //TODO: delete pfp
-    // usowa zdjecie pod sciezka <nickname_zalogowanego_uzytkownika>_pfp.png/jpg
+    // usowa zdjecie pod sciezka <nickname_zalogowanego_uzytkownika>_pfp.png/jpg/jpeg
 
     //TODO: add photo/video to message
 
