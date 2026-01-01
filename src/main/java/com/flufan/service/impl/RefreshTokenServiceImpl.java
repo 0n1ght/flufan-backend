2173f90d2ff4adaa37316f066e5353dbe82954b9
0n1ght <carlkasperek@proton.me>
2026-01-01 17:02:53 +0100

    fix: remove unnecessary lines

--- src/main/java/com/flufan/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/flufan/service/impl/RefreshTokenServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
